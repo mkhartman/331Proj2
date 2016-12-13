760b4b10cb38a87464f2a5d99182f83fe80a39c3
JLambert27 <alex27lambert@yahoo.com>
2016-12-13 16:16:58 -0500

    Changed Homepage
    Added Multiple Meetings to Homepage. Still needs to be fixed from there

--- AdvisingSite-master/src/advisor/views/homepage.php
+++ AdvisingSite-master/src/advisor/views/homepage.php
@@ -260,6 +260,78 @@ function findStudentsInMeeting($meetingID)
 
     </form>
 
+	    <form action="../utils/forms/createMeeting.php" method="POST">
+        <h4>
+            Create Multiple Meetings
+        </h4>
+        <ul>
+            <li>
+                <label>
+                    Meeting Start Date
+                    <input type="datetime-local" name="meetingStartTime">
+                </label>
+                <?php
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
+                }
+                ?>
+            </li>
+
+<li>
+                <label>
+                    Meeting End Date
+                    <input type="datetime-local" name="meetingEndTime">
+                </label>
+                <?php
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
+		  echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
+		  unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
+                }
+                ?>
+            </li>
+            <li>
+                <label>
+                    Building Name
+                    <input type="text" name="buildingName">
+                </label>
+                <?php
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_BUILDING"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"]);
+                }
+                ?>
+            </li>
+
+            <li>
+                <label>
+                    Room Number
+                    <input type="text" name="roomNumber">
+                </label>
+                <?php
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_ROOM"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"]);
+                }
+                ?>
+            </li>
+
+            <li>
+                <label>
+                    Type of Meeting:
+                    <select name="meetingType">
+                        <option value="group">Group</option>
+                        <option value="individual">Individual</option>
+                    </select>
+                </label>
+            </li>
+            <label>
+                <input type="submit">
+            </label>
+        </ul>
+
+    </form>
+	
 <?php } ?>
 
 </body>
