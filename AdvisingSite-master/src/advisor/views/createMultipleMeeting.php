003a5be3644c912a009349ee804e37547b8ce250
jennyz1 <jennyz1@umbc.edu>
2016-12-19 22:12:28 -0500

    Update createMultipleMeeting.php

--- AdvisingSite-master/src/advisor/views/createMultipleMeeting.php
+++ AdvisingSite-master/src/advisor/views/createMultipleMeeting.php
@@ -1,77 +1,81 @@
 <?php
+include ('header.php');
 session_start();
 
-// IF THERE ARE LOGGED IN
 if ($_SESSION["HAS_LOGGED_IN"]) {
   include '../utils/dbconfig.php';
-  include ('header.php');
 }
-?>
 
+?>
 <html>
 <head>
     <title>Advisor Homepage</title>
 </head>
 <body>
 <div class="container">
-    <form action="../utils/forms/createMultipleMeeting.php" method="POST">
-<h1>Create Multiple Meetings</h1>
+<h1>
+    Create A Single Meeting
+</h1>
 
-                        <li>
-                                <label>
-                                        Meeting Start Date
-                                        <input type="date" name="meetingStartDate">
-                                <?php
-  if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
-    echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
-    unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
-  }
+    <form action="../utils/forms/createMeeting.php" method="POST">
+
+        <ul>
+			
+			<li> 
+				<label>
+					Meeting Start Date
+					<input type="date" name="meetingStartDate">
+				<?php
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
+                }
                 ?>
-                                </label>
-                        </li>
-
-                        <li>
-                                <label>
-                                        Repeat every <input type="number" name="meetingRepeatDays"> days until <input type="date" name="meetingRepeatD">
-                                </label>
-                        </li>
-
-
+				</label>
+			</li>
+			
+			<li>
+				<label>
+					Repeat every <input type="number" name="meetingRepeatDays"> days until <input type="date" name="meetingRepeatD">
+				</label>
+			</li>
+				
+		
             <li>
                 <label>
                     Meeting Start Time
                     <input type="time" name="meetingStartTime">
                 </label>
                 <?php
-  if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
-    echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
-    unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
-  }
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"]);
+                }
                 ?>
             </li>
 
-                        <li>
+			<li>
                 <label>
-	    Meeting Length (Minutes)
+                    Meeting Length (Minutes)
                     <input type="number" name="meetingLength">
                 </label>
-                                <label>
-                                        Repeat every <input type="number" name="meetingRepeatTime"> minutes until <input type="time" name="meetingRepeat">
-                                </label>
+				<label>
+					Repeat every <input type="number" name="meetingRepeatTime"> minutes until <input type="time" name="meetingRepeat">
+				</label>
             </li>
-
+			
             <li>
                 <label>
                     Building Name
                     <input type="text" name="buildingName">
                 </label>
                 <?php
-  if (isset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"])) {
-    echo $_SESSION["ERROR_ADVISOR_MEETING_BUILDING"];
-    unset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"]);
-  }
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_BUILDING"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"]);
+                }
                 ?>
-                        </li>
+			</li>
 
             <li>
                 <label>
@@ -79,44 +83,42 @@ if ($_SESSION["HAS_LOGGED_IN"]) {
                     <input type="text" name="roomNumber">
                 </label>
                 <?php
-  if (isset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"])) {
-    echo $_SESSION["ERROR_ADVISOR_MEETING_ROOM"];
-    unset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"]);
-  }
+                if (isset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"])) {
+                    echo $_SESSION["ERROR_ADVISOR_MEETING_ROOM"];
+                    unset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"]);
+                }
                 ?>
-                        </li>
+			</li>
 
             <li>
                 <label>
-			Type of Meeting:
+                    Type of Meeting:
                     <select name="meetingType">
                         <option value="group">Group</option>
                         <option value="individual">Individual</option>
                     </select>
                 </label>
             </li>
-
-                        <li>
-                                <label>
-  Meeting Advisor:
-                                        <select name="advisor">
-  <?php foreach ($advisorRows as $aRow) { ?>
-					  <option value=<?php htmlspecialchars($aRow["advisorID"]) ?>><?php echo htmlspecialchars($aRow["firstName"]);
-					  echo htmlspecialchars($aRow["lastName"]); ?></option>
-					  <?php } ?>
-                                </label>
-                        </li>
-
-            <label>
+			
+			<li>
+				<label>
+					Meeting Advisor:
+					<select name="advisor">
+		                               <?php foreach ($advisorNames as $bRow) { ?>
+											//<?php echo '<option value=' . htmlspecialchars($bRow["advisorID"]) . '>' . htmlspecialchars($bRow["lastName"]) . '</option>' ?>
+											<option value="<?php echo htmlspecialchars($bRow["advisorID"]) ?>"> <?php echo htmlspecialchars($bRow["lastName"]); ?> </option>
+					       <?php } ?>
+		                        </select>
+				</label>
+			</li>
+			
+<label>
                 <input type="submit">
             </label>
-  </ul>
-  
+        </ul>
+
     </form>
-<br><br>
-<a href="homepage.php">Back to Homepage</a>
+ <a href="homepage.php">Back to Homepage</a>
 </div>
 </body>
 </html>
-
-
