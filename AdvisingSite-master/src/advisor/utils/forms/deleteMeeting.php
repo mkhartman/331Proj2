33e6b24fa723978cecbcc560df09aaa39ed2f5c1
jennyz1 <jennyz1@umbc.edu>
2016-12-20 01:48:27 -0500

    Add files via upload

--- AdvisingSite-master/src/advisor/utils/forms/deleteMeeting.php
+++ AdvisingSite-master/src/advisor/utils/forms/deleteMeeting.php
@@ -52,11 +52,13 @@ if ($_SESSION["HAS_LOGGED_IN"] and $_POST) {
       array_push($students, $stud);
     }
     foreach ($students as $student) {
+      $id=$student['StudentID'];
       $information = "SELECT * FROM Student 
-      WHERE id={$student['StudentID']}"; 
-      $open_connection->query($information);
-      echo $information['firstName']." ". $information['lastName']." - ".
-	$information['email']."<br>";
+      WHERE StudentID=$id"; 
+      $row = $open_connection->query($information);
+      $info = $row->fetch_assoc();
+      echo $info['firstName']." ". $info['lastName']." - ".
+	$info['email']."<br>";
     }
     echo "<br><b>Please notify these students at their email. There is a button on the button right that will open a new tab with gmail.";
 
@@ -72,6 +74,7 @@ if ($_SESSION["HAS_LOGGED_IN"] and $_POST) {
 ?>
 <br><br>
 <a target='_blank' href="https://www.gmail.com/" style="float:right;">Email</a>
+<a href="../../views/homepage.php">Back to Homepage</a>
 </div>
 </body>
 </html>
